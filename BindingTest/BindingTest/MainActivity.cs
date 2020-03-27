616e946d6a50eef014b93bff686495097a18c651
jair.pinedo <jair.pinedo@unosquare.com>
2020-03-27 13:53:27 -0600

    init project

---
+++ BindingTest/BindingTest/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
+using Com.Xamarin.Textcounter;
+
+namespace BindingTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        InputMethodManager imm;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            SetContentView(Resource.Layout.content_main);
+
+            imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+
+            var vowelsBtn = FindViewById<Button>(Resource.Id.vowels);
+            var consonBtn = FindViewById<Button>(Resource.Id.consonants);
+            var edittext = FindViewById<EditText>(Resource.Id.input);
+            edittext.InputType = Android.Text.InputTypes.TextVariationPassword;
+
+            edittext.KeyPress += (sender, e) =>
+            {
+                imm.HideSoftInputFromWindow(edittext.WindowToken, HideSoftInputFlags.NotAlways);
+                e.Handled = true;
+            };
+
+            vowelsBtn.Click += (sender, e) =>
+            {
+                int count = TextCounter.NumVowels(edittext.Text);
+                string msg = count + " vowels found.";
+                Toast.MakeText(this, msg, ToastLength.Short).Show();
+            };
+
+            consonBtn.Click += (sender, e) =>
+            {
+                int count = TextCounter.NumConsonants(edittext.Text);
+                string msg = count + " consonants found.";
+                Toast.MakeText(this, msg, ToastLength.Short).Show();
+            };
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
